904f43d8c6aba3b989c5e1703bc669020c569eba
DRL\preecjo <jon.preece@ao.com>
2018-07-04 11:43:44 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    main: './client/index'
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+              camelCase: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ],
+    noParse: [/\/react\//g, /\/react-dom\//g]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.css', '.scss'],
+    alias: {
+      react: 'react.setup.js',
+      'react-dom': 'react-dom.setup.js'
+    }
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      inject: 'body',
+      chunks: ['main']
+    }),
+    new webpack.ProvidePlugin({
+      React: 'React',
+      react: 'React',
+      'window.react': 'React',
+      'window.React': 'React'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css',
+      chunkFilename: '[id].bundle.css'
+    })
+  ]
+}
